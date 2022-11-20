c98b11b9e218c3b16bae8d7c778cc4a1bca3d99c
Qwanjai Deelertpradit <d_khonjai@hotmail.com>
2022-11-21 00:03:58 +0700

    fix : article createdate

--- src/main/java/int371/namjai/domain/article/ArticleRepository.java
+++ src/main/java/int371/namjai/domain/article/ArticleRepository.java
@@ -12,6 +12,6 @@ public interface ArticleRepository extends JpaRepository<Article, String> {
 
     List<Article> findArticlesByUser_EmailOrderByCreateDateAsc(String email);
 
-    @Query(value = "select * from articles a where a.status ='OPEN' order by a.create_date desc limit 3 ", nativeQuery = true)
+    @Query(value = "select * from articles a where a.status ='OPEN' and a.create_date >='2022-11-20 00:00:00' and a.create_date<'2022-11-21 00:00:00' order by a.create_date desc limit 3 ", nativeQuery = true)
     List<Article> findTop3AndStatusOpen();
 }
